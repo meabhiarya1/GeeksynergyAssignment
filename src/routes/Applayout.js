d5cee859a27ba3aa30cf57d5904fa100b0471e75
Abhishek kumar <90932403+meabhiarya1@users.noreply.github.com>
2023-11-11 00:39:55 +0530

    routing added

--- src/routes/Applayout.js
+++ src/routes/Applayout.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react":
 import Authenticated from "./authenticated";
 import Unauthenticated from "./unauthenticated";
 
